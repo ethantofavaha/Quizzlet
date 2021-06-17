82c4febb2d233731ff8ee768cdb879f4c7fe7422
ethantofavaha <80223973+ethantofavaha@users.noreply.github.com>
2021-06-18 09:56:10 +1200

    Create Quiz.py

---
+++ Quiz.py
@@ -0,0 +1,12 @@
+#multiple question
+print("What is the best fastfood franchise?")
+user_answer = input()
+
+if user_answer == "Mcdonalds":
+  print("You are correct!")
+elif user_answer == "Burger King":
+  print("its nice but not the best.")
+else:
+  print("not even close, try again next time.")
+print("What is the best fastfood franchise?")
+user_answer = input()
